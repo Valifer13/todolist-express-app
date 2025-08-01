cb8ce6c5601cc98717f8278b8654d2c983b1c35e
Valifer13 <vianviszello@gmail.com>
2025-08-01 21:43:02 +0800

    backend - add cors middleware package

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,13 @@
 import { app } from "./app";
 import { stdout } from "process";
+// import cors from "cors";
 
 const port = process.env.PORT || 3000;
+
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   credentials: true,
+// }));
 
 app.listen(port, () => {
   stdout.write(`Server is running at http://localhost:${port}\n`)
